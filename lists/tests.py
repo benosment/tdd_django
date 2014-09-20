1cea0bdee85d3c169dd00753f4157648a2a54837
Ben Osment <benjamin.j.osment@gmail.com>
2014-09-20 09:41:47 -0400

    added a new unit test to make sure items get added to the database

--- lists/tests.py
+++ lists/tests.py
@@ -26,6 +26,12 @@ class HomePageTest(TestCase):
         request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string('home.html',
             {'new_item_text': 'A new list item'})
 
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from lists.models import Item
 
 
 def home_page(request):
+    if (request.method == 'POST'):
+        new_item = Item()
+        new_item.text = request.POST.get('item_text')
+        new_item.save()
     return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', ''), })
