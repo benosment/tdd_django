fe7c71b5dae8d69bcd9a361e58d870f62046f342
Ben Osment <benjamin.j.osment@gmail.com>
2014-09-21 20:01:45 -0400

    changed adding a new list item to a more RESTful approach

--- lists/tests.py
+++ lists/tests.py
@@ -21,32 +21,6 @@ class HomePageTest(TestCase):
 
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_can_save_a_post_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        home_page(request)
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_home_page_redirects_after_post(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)  # is there a define for what these statuses are?
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModelTest(TestCase):
 
@@ -82,3 +56,18 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'item1')
         self.assertContains(response, 'item2')
+
+
+class NewListTest(TestCase):
+
+    def test_saving_a_post_request(self):
+        self.client.post('/lists/new',
+                         data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_post(self):
+        response = self.client.post('/lists/new',
+                                     data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
