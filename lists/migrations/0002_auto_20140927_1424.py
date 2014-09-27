b5f5be62239ff6fe5be114b7f239f840e13ad253
Ben Osment <benjamin.j.osment@gmail.com>
2014-09-27 13:55:04 -0400

    refactored the lists URLs

---
+++ lists/migrations/0002_auto_20140927_1424.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.ForeignKey(to='lists.List', default=None),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='text',
+            field=models.TextField(default=''),
+            preserve_default=True,
+        ),
+    ]
